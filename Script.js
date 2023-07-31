c9ca699f9e744c135f056cee83c9dd0d714f110f
avayyyyyyy <jabhi465@gmail.com>
2023-07-31 18:14:32 +0530

    on the wayyyyy!

--- Script.js
+++ Script.js
@@ -18,10 +18,27 @@
 // async js hai kya ?
 // kai baar hamren saath aisa hoota hai kki hume nhi pta hoota ki servr se info hamare pas kah tak ayegi....toh uss case me hum sync js use mhi kr sakte isse nipatne ke liye hum og async code likh dete hai taaki blocking naa ho and jabh bhi ans aaye hamara answe ke respect me chalne waaala code chal jaaye!!!
 
+// now the following line will execute after 2sec.
+// setTimeout(() => {
+//   console.log("Hello bacho!");
+// }, 12000);
+// console.log("Hello bacho!2");
+
 // js is not asynchronous
 // async ki poori kahani
 // single threading vs multiple threads
+
 // callbacks
+// callback function woh funcion hoote hai jinko hum thode time ke baad waapis call back kiya jaata hai !
+
+// in this print is the call back function which is called after a certain time when it is called again.
+setTimeout(() => {
+  print(); //callback function called.
+}, 5000);
+
+function print() {
+  console.log("Hello bacho!2");
+}
 // promises
 // then and catch
 // try and catch
