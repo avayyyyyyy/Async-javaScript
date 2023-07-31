26c7bb41ef6790eedf0c624e31dbddfd1af37d60
avayyyyyyy <jabhi465@gmail.com>
2023-07-31 15:18:24 +0530

    Ayncs kya hai?

---
+++ Script.js
@@ -0,0 +1,17 @@
+// Sync Ascync me kya fark hoota hai ?
+// sync me ek ke baad ek kaam hoota hai jab tak ek command execute nhi hooga dusra start nhi hooga, pr async me koi bhi kaam kabhi bhi ho sakta hai!
+
+// task a = 5;
+// task b = 2;
+// task c = 15;
+// task d = 1;
+
+// async js hai kya ?
+// js is not asynchronous
+// async ki poori kahani
+// single threading vs multiple threads
+// callbacks 
+// promises
+// then and catch
+// try and catch
+// async await
