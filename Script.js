14b62a992e5af5d3090f3b40c5ed58cdab66883d
avayyyyyyy <jabhi465@gmail.com>
2023-07-31 15:29:06 +0530

    on the wayyyy!

--- Script.js
+++ Script.js
@@ -6,6 +6,16 @@
 // task c = 15;
 // task d = 1;
 
+
+// kaise pata chalta hai ki code sync hai yaa async ?
+// if you are using any of these in your code then your code is asynchronous : 
+// 1. setTimeout
+// 2. setInterval
+// 3. fetch
+// 4. axios
+// 5. Promise
+// 6. XMLHttpRequest
+
 // async js hai kya ?
 // js is not asynchronous
 // async ki poori kahani
